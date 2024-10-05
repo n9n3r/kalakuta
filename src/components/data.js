620897c9164c55cacffa80b2d292281f0cddbe70
n9n3r <kloudn9ner@gmail.com>
2024-10-05 06:55:27 +0100

    test 3

--- src/components/data.js
+++ src/components/data.js
@@ -2,7 +2,7 @@ const menu = [
     {
       id: 1,
       title: 'buttermilk pancakes',
-      category: 'breakfast',
+      category: 'breakfast & brunch',
       price: 15.99,
       img: './assets/img/1.jpg',
       desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
@@ -10,7 +10,7 @@ const menu = [
     {
       id: 2,
       title: 'diner double',
-      category: 'lunch',
+      category: 'continental',
       price: 13.99,
       img: './img/2.jpg',
       desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
@@ -18,7 +18,7 @@ const menu = [
     {
       id: 3,
       title: 'godzilla milkshake',
-      category: 'shakes',
+      category: 'drinks',
       price: 6.99,
       img: './img/3.jpg',
       desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
@@ -26,7 +26,7 @@ const menu = [
     {
       id: 4,
       title: 'country delight',
-      category: 'breakfast',
+      category: 'breakfast & brunch',
       price: 20.99,
       img: './img/2.jpe',
       desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
@@ -34,7 +34,7 @@ const menu = [
     {
       id: 5,
       title: 'egg attack',
-      category: 'lunch',
+      category: 'swallow',
       price: 22.99,
       img: './img/1.jpg',
       desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
@@ -42,7 +42,7 @@ const menu = [
     {
       id: 6,
       title: 'oreo dream',
-      category: 'shakes',
+      category: 'drinks',
       price: 18.99,
       img: './img/3.jpeg',
       desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
@@ -50,7 +50,7 @@ const menu = [
     {
       id: 7,
       title: 'bacon overflow',
-      category: 'breakfast',
+      category: 'nigerian specials',
       price: 8.99,
       img: './img/2.jpg',
       desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
@@ -58,7 +58,7 @@ const menu = [
     {
       id: 8,
       title: 'american classic',
-      category: 'lunch',
+      category: 'continental',
       price: 12.99,
       img: './img/1.jpg',
       desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
@@ -66,7 +66,7 @@ const menu = [
     {
       id: 9,
       title: 'quarantine buddy',
-      category: 'shakes',
+      category: 'drinks',
       price: 16.99,
       img: './img/2.jpeg',
       desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
