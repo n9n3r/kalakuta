29cbcb368eacf18e946b25c13938f6180c6f0f2c
n9n3r <kloudn9ner@gmail.com>
2024-10-05 06:33:42 +0100

    test 3

--- src/components/Lodge.jsx
+++ src/components/Lodge.jsx
@@ -7,13 +7,13 @@ const Lodge = () => {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center lg:px-32 px-5">
       <h1 className=" text-4xl font-semibold text-center lg:pt-8 pt-24 pb-10">
-        Premium Menu
+        Premium Accomodations
       </h1>
 
       <div className=" flex flex-wrap gap-8 justify-center">
-        <DishesCard img={menu1} title="Delicious Dish" price="$16.99" />
-        <DishesCard img={menu2} title="Delicious Dish" price="$18.99" />
-        <DishesCard img={menu3} title="Delicious Dish" price="$14.99" />
+        <DishesCard img={menu1} title="Pristine Rooms" price="$16.99" />
+        <DishesCard img={menu2} title="Comfort at it's best" price="$18.99" />
+        <DishesCard img={menu3} title="Elegance" price="$14.99" />
       </div>
     </div>
   );
